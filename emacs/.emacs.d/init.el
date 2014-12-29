3b9b076eb97681260af401c2a1f0fce589629c28
shellhead <mike.hunsinger@gmail.com>
2014-12-28 17:02:18 -0700

    Changes to init and packages files

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,82 +1,90 @@
-;; init.el --- Initialization file for Emacs
-;; Many thanks to Lee for helping me with Emacs
+;; init.el --- A masterpiece in progress
+;; Many thanks to Lee for helping me with this
 
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
-(setq package-enable-at-startup nil)
 
 (defconst f '(-1))
 
 ;; Default emacs settings
-(setq inhibit-startup-message      t
-      initial-major-mode           'fundamental-mode)
-(setq mac-command-modifier 'super)
-(setq ring-bell-function           (lambda()))
-(put 'erase-buffer                 'disabled nil)
+(setq package-enable-at-startup  nil
+      inhibit-startup-message    t
+      initial-major-mode         'fundamental-mode
+      ring-bell-function         (lambda())
+      indent-tabs-mode           nil)
 
 ;; File reading/writing settings
-(setq read-file-name-completion-ignore-case t)
-(setq delete-auto-save-files                t)
-(setq find-file-visit-truename              nil)
+(setq read-file-name-completion-ignore-case  t
+      delete-auto-save-files                 t
+      find-file-visit-truename               nil
+      vc-follow-symlinks                     t
+      electric-indent-mode                   t)
 
 ;; Appearances
-(load-theme 'smyx                  t)
-(global-font-lock-mode             t)
-(line-number-mode                  t)
-(column-number-mode                t)
-(transient-mark-mode               t)
-(menu-bar-mode                     f)
-(tool-bar-mode                     f)
-(blink-cursor-mode                 f)
-
-(setq vc-follow-symlinks           t)
-(setq make-pointer-invisible       t)
-(setq fill-column                  80)
-(setq echo-keystrokes              0.1)
-(setq vc-handled-backends          '(SVN Git))
+(load-theme 'smyx             t)
+(global-font-lock-mode        t)
+(line-number-mode             t)
+(column-number-mode           t)
+(transient-mark-mode          t)
+(menu-bar-mode                f)
+(tool-bar-mode                f)
+(blink-cursor-mode            f)
+(setq make-pointer-invisible  t
+      fill-column             80
+      echo-keystrokes         0.1
+      vc-handled-backends     '(SVN Git))
 
 ;; Key Bindings
-(global-set-key (kbd "C-s")        'isearch-forward-regexp)
-(global-set-key (kbd "C-r")        'isearch-backward-regexp)
-(global-set-key (kbd "M-%")        'query-replace-regexp)
-(global-set-key (kbd "C-x C-l")    'toggle-truncate-lines)
-(global-set-key (kbd "C-x +")      'balance-windows-area)
-(global-set-key (kbd "RET")        'newline-and-indent)
-(global-set-key (kbd "C-t")        'Control-X-prefix)
+(global-set-key (kbd "C-s")      'isearch-forward-regexp)
+(global-set-key (kbd "C-r")      'isearch-backward-regexp)
+(global-set-key (kbd "M-%")      'query-replace-regexp)
+(global-set-key (kbd "C-x C-l")  'toggle-truncate-lines)
 
+;; Change this around for Dvorak
+(define-key key-translation-map "C-x" "C-t")
+
+;; Enable stock disabled commands
+(put 'erase-buffer 'disabled nil)
+
+;; If you actually enjoy writing yes/no, you should be beaten with a hose
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-;; Custom variable
-(custom-set-variables
- '(nxml-child-indent 4)
- '(nxml-slash-auto-complete-flag t))
+(defun my/add-watchwords ()
+  "Highlight FIXME and TODO in code"
+  (font-lock-add-keywords
+   nil '(("\\<\\(FIXME\\|TODO\\)\\>"
+          1 '((:foreground "#DC8CC3")) t))))
+(add-hook 'prog-mode-hook 'my/add-watchwords)
 
-;; General settings that should be applied to all programming modes
-(add-hook 'prog-mode-hook
-          (lambda ()
-            (visual-line-mode      t)
-            (subword-mode          t)))
-
-;; C
 (defun my/c-mode-init()
-  (c-set-style                     "k&r")
-  (c-toggle-electric-state         f)
-  (hs-minor-mode                   t)
-  (setq c-basic-offset             4))
+  "Various style settings for C."
+  (c-set-style              "k&r")
+  (c-toggle-electric-state  f)
+  (setq c-basic-offset      4))
 (add-hook 'c-mode-hook #'my/c-mode-init)
 
 ;; Go
 (defun my/go-mode-init()
-  (setq c-basic-offset             8
-        indent-tabs-mode           t))
+  "Various style settings for Go."
+  (setq c-basic-offset    8
+        indent-tabs-mode  t))
 (add-hook 'go-mode-hook #'my/go-mode-init)
 
 ;; XML
 (defun my/nxml-mode-init()
-  (setq subword-mode t))
+  "Various style settings for nXML."
+  (setq subword-mode                   t
+        nxml-child-indent              4
+        nxml-slash-auto-complete-flag  t))
 (add-hook 'nxml-mode-hook #'my/nxml-mode-init)
+
+;; General settings that should be applied to all programming modes
+(add-hook 'prog-mode-hook
+          (lambda ()
+            (visual-line-mode  t)
+            (subword-mode      t)))
 
 ;; Sync Emacs' kill ring and Mac's copy buffer
 (when (eq system-type 'darwin)
@@ -90,30 +98,55 @@
   (setq interprogram-cut-function 'paste-to-osx
         interprogram-paste-function 'copy-from-osx))
 
-;; stop putting temp files in the same directory, if I need it put it elsewhere
+;; Stop putting temp files in the same directory, if I need it put it elsewhere
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-(defun beautify-json ()
+(defun pretty-print-json ()
+  "Pretty print JSON."
   (interactive)
   (let ((b (if mark-active (min (point) (mark)) (point-min)))
         (e (if mark-active (max (point) (mark)) (point-max))))
     (shell-command-on-region b e
-                             "python -mjson.tool" (current-buffer) t)))
+                             "python -mjson.tool " (current-buffer) t)))
 
-;; Remove tabs from the entire buffer
+(defun pretty-print-xml ()
+  "Pretty print XML."
+  (interactive)
+  (let ((b (if mark-active (min (point) (mark)) (point-min)))
+        (e (if mark-active (max (point) (mark)) (point-max))))
+    (shell-command-on-region b e
+                             "xmllint --format -" (current-buffer) t))
+  (indent-buffer))
+
 (defun untabify-buffer()
+  "Replace all tabs with spaces."
   (interactive)
   (untabify (point-min) (point-max)))
 
-;; Indents entire buffer, be careful doing this on large files
 (defun indent-buffer()
+  "Indent the buffer."
   (interactive)
   (indent-region (point-min) (point-max)))
 
-;; Packages
+(defun cleanup-buffer ()
+  "Indent, untabify, and delete trailing whitespace."
+  (interactive)
+  (indent-buffer)
+  (untabify-buffer)
+  (delete-trailing-whitespace))
+
+;; TODO need a better way of finding ox-gfm, regardless of version number
+(defun my/load-github-flavored-markdown ()
+  "Use github's flavored markdown when exporting org files."
+  (interactive)
+  (let (ox-package (concat user-emacs-directory "/elpa/ox-gfm-20141211.240"))
+    (when (file-exists-p ox-package)
+      (load-file (concat ox-package "/ox-gfm.el")))))
+
+;; packages
 (require 'use-package)
 
 ;; uniquify
@@ -145,17 +178,9 @@
   :init
   (add-hook 'prog-mode-hook 'smartparens-mode t))
 
-;; smex
-(use-package smex
-  :bind
-  ("M-x" . smex)
-  ("M-X" . smex-major-mode-commands)
-  :config
-  (setq smex-save-file (concat user-emacs-directory "misc/smex/.smex-items")))
-
 ;; expand-region
 (use-package expand-region
-  :bind (("C-c e" . er/expand-region)
+  :bind (("C-c e"   . er/expand-region)
          ("C-c C-e" . er/contract-region))
   :config
   (pending-delete-mode t))
@@ -164,9 +189,9 @@
 (use-package hideshow
   :bind
   ("C-c TAB" . hs-toggle-hiding)
-  ("C-\\" . hs-toggle-hiding)
-  ("M-\\" . hs-hide-all)
-  ("M-|" . hs-show-all)
+  ("C-\\"    . hs-toggle-hiding)
+  ("M-\\"    . hs-hide-all)
+  ("M-|"     . hs-show-all)
   :init
   (add-hook 'prog-mode-hook 'hs-minor-mode t))
 
@@ -175,42 +200,19 @@
   :init
   (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
-;; ido
-(use-package ido
-  :init
-  (ido-mode t)
-  :config
-  (progn
-    (setq ido-use-virtual-buffers                nil
-          ido-enable-prefix                      nil
-          ido-enable-flex-matching               t
-          ido-auto-merge-work-directories-length nil
-          ido-create-new-buffer                  'always
-          ido-save-directory-list-file
-          (concat user-emacs-directory "misc/ido/.ido-list")))
-  (use-package flx-ido
-    :init
-    (flx-ido-mode t)
-    :config
-    (setq ido-use-faces nil))
-  (use-package ido-vertical-mode
-    :init
-    (ido-vertical-mode t)))
-
 ;; company
 (use-package company
   :init
-  (setq company-idle-delay            0.1
-	company-minimum-prefix-length 3
-	company-selection-wrap-around t
-	company-show-numbers          t
-	company-dabbrev-downcase      nil
-	company-transformers          '(company-sort-by-occurrence))
+  (setq company-idle-delay             0.1
+        company-minimum-prefix-length  3
+        company-selection-wrap-around  t
+        company-dabbrev-downcase       nil
+        company-transformers           '(company-sort-by-occurrence))
   (bind-keys :map company-active-map
-  	     ("C-n"      . company-select-next)
-  	     ("C-p"      . company-select-previous)
-  	     ("C-d"      . company-show-doc-buffer)
-  	     ("<tab>"    . company-complete))
+             ("C-n"   . company-select-next)
+             ("C-p"   . company-select-previous)
+             ("C-d"   . company-show-doc-buffer)
+             ("<tab>" . company-complete))
   (add-hook 'prog-mode-hook 'company-mode))
 
 ;; popwin
@@ -227,32 +229,32 @@
     (push '(" *undo-tree*" :stick t :height 20 :position bottom :noselect t)
           popwin:special-display-config)
     (push '("*Comile-Log*" :stick f :noselect t)
-          popwin:special-display-config)
-    (push '("*eshell*" :stick t :height 20 :position bottom :noselect t)
           popwin:special-display-config)))
 
 ;; git-gutter
 (use-package git-gutter
-  :idle (git-gutter-mode t)
-  :bind (("C-x =" . git-gutter:popup-hunk)
-         ("C-c P" . git-gutter:previous-hunk)
-         ("C-c N" . git-gutter:next-hunk)
-         ("C-x p" . git-gutter:previous-hunk)
-         ("C-x n" . git-gutter:next-hunk)
-         ("C-c G" . git-gutter:popup-hunk)))
+  :idle
+  (git-gutter-mode t)
+  :bind
+  (("C-x =" . git-gutter:popup-hunk)
+   ("C-c P" . git-gutter:previous-hunk)
+   ("C-c N" . git-gutter:next-hunk)
+   ("C-x p" . git-gutter:previous-hunk)
+   ("C-x n" . git-gutter:next-hunk)
+   ("C-c G" . git-gutter:popup-hunk)))
 
 ;; flycheck
 (use-package flycheck
   :init
   (flycheck-mode)
   (setq-default flycheck-disabled-checkers
-		'(emacs-lisp-checkdoc))
+                '(emacs-lisp-checkdoc))
   (use-package flycheck-tip
     :config
     (add-hook 'flycheck-mode-hook
-	      (lambda ()
-		(global-set-key (kbd "C-c C-n") 'flycheck-tip-cycle)
-		(global-set-key (kbd "C-c C-p") 'flycheck-tip-cycle-reverse)))))
+              (lambda ()
+                (global-set-key (kbd "C-c C-n") 'flycheck-tip-cycle)
+                (global-set-key (kbd "C-c C-p") 'flycheck-tip-cycle-reverse)))))
 
 ;; js2-mode
 (use-package js2-mode
@@ -261,22 +263,22 @@
 ;; dired
 (use-package dired
   :bind
-  ("C-c ." . dired-jump)
+  ("C-x C-j" . dired-jump)
   :config
-    (use-package dired-x
-      :config
-      (when (eq system-type 'darwin)
-	(add-to-list 'dired-omit-extensions ".DS_Store")
-	(setq insert-directory-program "/usr/local/bin/gls")
-	(setq dired-listing-switches "-aBhl --group-directories-first"))
-      (setq ls-lisp-dirs-first t
-	    delete-by-moving-to-trash t
-	    dired-dwim-target t)
-      (define-key dired-mode-map (kbd "C-c C-u") 'dired-up-directory)
-      (define-key dired-mode-map (kbd "C-x C-q") 'wdired-change-to-wdired-mode)
-      (add-hook 'dired-mode-hook (lambda ()
-				   (hl-line-mode t)
-				   (toggle-truncate-lines t)))))
+  (use-package dired-x
+    :config
+    (when (eq system-type 'darwin)
+      (add-to-list 'dired-omit-extensions ".DS_Store")
+      (setq insert-directory-program "/usr/local/bin/gls")
+      (setq dired-listing-switches "-aBhl --group-directories-first"))
+    (setq ls-lisp-dirs-first         t
+          delete-by-moving-to-trash  t
+          dired-dwim-target          t)
+    (define-key dired-mode-map (kbd "C-c C-u") 'dired-up-directory)
+    (define-key dired-mode-map (kbd "C-x C-q") 'wdired-change-to-wdired-mode)
+    (add-hook 'dired-mode-hook (lambda ()
+                                 (hl-line-mode t)
+                                 (toggle-truncate-lines t)))))
 
 ;; golden-ratio
 (use-package golden-ratio
@@ -285,7 +287,110 @@
 
 ;; multiple-cursors
 (use-package multiple-cursors
-  :bind (("C-S-c C-S-c" . mc/edit-lines)
-         ("C-c >" . mc/mark-next-like-this)
-         ("C-c <" . mc/mark-previous-like-this)
-         ("C-c C-a" . mc/mark-all-like-this)))
+  :bind
+  (("C-S-c C-S-c" . mc/edit-lines)
+   ("C-c >"       . mc/mark-next-like-this)
+   ("C-c <"       . mc/mark-previous-like-this)
+   ("C-c C-a"     . mc/mark-all-like-this)))
+
+;; helm
+(use-package helm
+  :bind
+  (("C-x b"   . helm-mini) ; replace normal buffer window
+   ("C-x k"   . helm-mini) ; replace kill buffer options
+   ("M-x"     . helm-M-x)
+   ("C-x C-i" . helm-semantic-or-imenu)
+   ("M-y"     . helm-show-kill-ring)
+   ("C-x f"   . helm-find-files)
+   ("C-h b"   . helm-descbinds))
+  :config
+  (progn
+    (use-package helm-files)
+    (use-package helm-config)
+    (setq helm-buffers-fuzzy-matching  t
+          helm-truncate-lines          t)
+    (use-package helm-swoop
+      :bind
+      (("M-i" . helm-swoop)
+       ("M-I" . helm-multi-swoop)))
+    (use-package helm-descbinds
+      :init
+      (helm-descbinds-mode t))))
+
+;; eww
+(use-package eww
+  :bind
+  (("C-c w" . eww)
+   ("C-c o" . eww-browse-with-external-browser)))
+
+;; ido
+(use-package ido
+  :init
+  (ido-mode t)
+  :config
+  (progn
+    (setq ido-use-virtual-buffers                 nil
+          ido-enable-prefix                       nil
+          ido-enable-flex-matching                t
+          ido-auto-merge-work-directories-length  nil
+          ido-create-new-buffer                   'always
+          ido-save-directory-list-file
+          (concat user-emacs-directory "misc/ido/.ido-list")))
+  (use-package flx-ido
+    :init
+    (flx-ido-mode t)
+    :config
+    (setq ido-use-faces nil))
+  (use-package ido-vertical-mode
+    :init
+    (ido-vertical-mode t)))
+
+;; org
+(use-package org
+  :bind
+  (("C-c a" . org-agenda)
+   ("C-c c" . org-capture)
+   ("C-c b" . org-iswitchb)
+   ("C-c l" . org-store-link))
+  :config
+  (progn
+    ;; Setup org directories
+    (setq org-directory         "~/Org")
+    (setq org-agenda-files      '("~/Org/personal.org"
+				  "~/Org/work.org"
+				  "~/Org/school.org"))
+
+    ;; capture settings
+    (setq org-capture-templates '(("t" "To do" entry (file "~/Org/refile.org")
+                                   "* TODO %?\n%U\n")))
+    (setq org-refile-targets    '((nil :maxlevel . 3)
+                                  (org-agenda-files :maxlevel . 3)))
+    (setq org-refile-use-outline-path             t
+          org-refile-allow-creating-parent-nodes  'confirm)
+
+    ;; todo settings
+    (setq org-todo-keywords            '(("TODO(t)" "INPROGRESS(i)" "BLOCKED(b)" "DONE(d)"))
+          org-use-fast-todo-selection  t
+          org-refile-use-outline-path  t
+          ido-everywhere               t)
+
+    ;; miscellaneous settings
+    (setq org-list-allow-alphabetical  t)
+
+    ;; key mappings
+    (define-key org-mode-map (kbd "C-c t") 'org-todo)
+
+    (add-hook 'org-mode-hook (lambda ()
+			       (setq fill-column 80)
+			       (turn-on-auto-fill)
+			       (turn-on-flyspell)))))
+
+;; Graveyard, where old config goes to die.
+;; smex
+;; Trying out helm for now
+;; (use-package smex
+;;   :bind
+;;   ("M-x" . smex)
+;;   ("M-X" . smex-major-mode-commands)
+;;   :config
+;;   (setq smex-save-file (concat user-emacs-directory "misc/smex/.smex-items")))
--- emacs/.emacs.d/misc/packages.el
+++ emacs/.emacs.d/misc/packages.el
@@ -1,4 +1,4 @@
-;; I stole this from dakrone, shhhhhhhhh
+;; Download the packages used in init
 (require 'package)
 (setq package-user-dir (concat user-emacs-directory "elpa"))
 (add-to-list 'package-archives 
@@ -8,28 +8,30 @@
 
 (defvar my/install-packages
   '(use-package
-     uniquify
      smooth-scrolling
-     undo-tree
-     magit
-     smartparens
+     undo-tree             ; visualize undo tree
+     magit                 ; git interface
+     smartparens           ; balance parens
      smex
      expand-region
      hideshow
-     rainbow-delimiters
+     rainbow-delimiters    ; color parens and braces
      ido
      flx-ido
      ido-vertical-mode
-     company
+     company               ; complete any
      popwin
      git-gutter
      flycheck
      flycheck-tip
-     js2-mode
-     dired
+     js2-mode              ; a saner way of editing javascript
      dired-x
-     golden-ratio
-     multiple-cursors)
+     golden-ratio          ; balance windows 
+     multiple-cursors
+     web                   ; http client
+     helm                  ; incremental completion
+     helm-descbinds        ; search current keybindings
+     helm-swoop)           ; search by pattern in files
 
   (dolist (pack my/install-packages)
     (unless (package-installed-p pack)
