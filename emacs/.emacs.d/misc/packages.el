482109b5344532fb865c9add94feeb19b3c73a49
shellhead <mike.hunsinger@gmail.com>
2015-01-20 22:54:24 -0700

    Initial commit on Arch box

---
+++ emacs/.emacs.d/misc/packages.el
@@ -0,0 +1,33 @@
+;; Download the packages used in init
+(require 'package)
+(setq package-user-dir (concat user-emacs-directory "elpa"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+(package-initialize)
+(package-refresh-contents)
+
+(defvar my/install-packages
+  '(company
+    expand-region
+    flx-ido
+    git-gutter
+    helm
+    helm-descbinds
+    helm-swoop
+    hideshow
+    ido
+    ido-vertical-mode
+    js2-mode
+    magit
+    multiple-cursors
+    popwin
+    rainbow-delimiters
+    smartparens
+    smex
+    smooth-scrolling
+    undo-tree
+    use-package))
+
+(dolist (pack my/install-packages)
+  (unless (package-installed-p pack)
+    (package-install pack)))
---
+++ zsh/.zshrc
@@ -0,0 +1,20 @@
+# config
+autoload -U compinit && compinit
+autoload -U colors && colors
+
+setopt dvorak
+setopt correct
+
+HISTFILE=$HOME/.zsh-history
+HISTSIZE=1000
+SAVEHIST=1000
+
+# aliases
+alias l='ls -Gh'
+alias emacs='emacs -nw'
+alias rm='rm -i'
+alias ..='cd ..'
+alias magit='emacs -f magit-status'
+
+# prompt
+PROMPT='%3~ : '
