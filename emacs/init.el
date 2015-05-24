8f7292ef7d5f70361cebd39657fc86d616a3ea8e
shellhead <mike.hunsinger@gmail.com>
2015-05-24 01:11:55 -0600

    Added 'ensure' to emacs' packages

--- emacs/init.el
+++ emacs/init.el
@@ -167,6 +167,7 @@
   :config (setq uniquify-buffer-name-style 'post-forward-angle-brackets))
 
 (use-package smooth-scrolling
+  :ensure t
   :config (setq smooth-scroll-margin 8))
 
 (use-package undo-tree
@@ -174,10 +175,12 @@
   :bind ("C-x u" . undo-tree-visualize))
 
 (use-package magit
+  :ensure t
   :bind ("M-g M-g" . magit-status)
   :config (setq magit-auto-rever-mode nil))
 
 (use-package smartparens
+  :ensure t
   :diminish smartparens-mode
   :bind (("C-c (" . sp-forward-barf-sexp)
          ("C-c )" . sp-forward-slurp-sexp))
@@ -185,11 +188,13 @@
   :init (add-hook 'prog-mode-hook 'smartparens-mode t))
 
 (use-package expand-region
+  :ensure t
   :bind (("C-c e"   . er/expand-region)
          ("C-c C-e" . er/contract-region))
   :config (pending-delete-mode t))
 
 (use-package hideshow
+  :ensure t
   :bind (("C-c TAB" . hs-toggle-hiding)
          ("C-\\"    . hs-toggle-hiding)
          ("M-\\"    . hs-hide-all)
@@ -197,20 +202,24 @@
   :init (add-hook 'prog-mode-hook 'hs-minor-mode t))
 
 (use-package rainbow-delimiters
+  :ensure t
   :init (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
 (use-package company
+  :ensure t
   :diminish company-mode
   :init (add-hook 'prog-mode-hook 'company-mode)
-  :config (setq company-idle-delay 0.1
-                company-minimum-prefix-length 3
-                company-selection-wrap-around t
-                company-dabbrev-downcase nil
-                company-transformers '(company-sort-by-occurrence)))
+  :config
+  (setq company-idle-delay 0.1
+        company-minimum-prefix-length 3
+        company-selection-wrap-around t
+        company-dabbrev-downcase nil
+        company-transformers '(company-sort-by-occurrence)))
 
 (use-package popwin
+  :ensure t
+  :init (popwin-mode t)
   :config
-  (popwin-mode t)
   (progn
     (defvar popwin:special-display-config-backup popwin:special-display-config)
     (setq display-buffer-function 'popwin:display-buffer)
@@ -223,16 +232,8 @@
     (push '("*Comile-Log*" :stick f :noselect t)
           popwin:special-display-config)))
 
-(use-package git-gutter
-  :bind
-  (("C-x =" . git-gutter:popup-hunk)
-   ("C-c P" . git-gutter:previous-hunk)
-   ("C-c N" . git-gutter:next-hunk)
-   ("C-x p" . git-gutter:previous-hunk)
-   ("C-x n" . git-gutter:next-hunk)
-   ("C-c G" . git-gutter:popup-hunk)))
-
 (use-package js2-mode
+  :ensure t
   :mode "\\.js\\'"
   :config (setq-default js2-basic-offset 2))
 
@@ -249,6 +250,7 @@
                                          (toggle-truncate-lines t)))))
 
 (use-package helm
+  :ensure t
   :bind (("C-x b"   . helm-mini)
          ("C-x k"   . helm-mini)
          ("M-x"     . helm-M-x)
@@ -270,11 +272,11 @@
               (helm-descbinds-mode t))))
 
 (use-package eww
-  :bind
-  (("C-c w" . eww)
-   ("C-c o" . eww-browse-with-external-browser)))
+  :bind (("C-c w" . eww)
+         ("C-c o" . eww-browse-with-external-browser)))
 
 (use-package ido
+  :ensure t
   :init (ido-mode t)
   :config
   (progn
@@ -292,6 +294,7 @@
     :init (ido-vertical-mode t)))
 
 (use-package org
+  :ensure t
   :bind (("C-c a" . org-agenda)
          ("C-c c" . org-capture)
          ("C-c b" . org-iswitchb)
