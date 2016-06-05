ef17fa61921c2891110219f118ca9e7eac6f8133
Michael Hunsinger <mike.hunsinger@gmail.com>
2016-06-05 00:03:20 -0600

    Added company configuration.

--- emacs/init.el
+++ emacs/init.el
@@ -1,9 +1,7 @@
 ;; Package initialization 
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages")) ; Add MELPA packages. 
-(add-to-list 'package-archives
-	     '("org" . "https://orgmode.org/elpa")) ; Add org packages.
+	     '("melpa" . "https://melpa.org/packages/")) ; Add MELPA packages. 
 (package-initialize)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -28,6 +26,7 @@
       "cl" 'comment-indent
       ;; Helm
       "a"  'helm-apropos
+      "k"  'helm-descbinds
       "m"  'helm-man-woman
       "x"  'helm-M-x
       "f"  'helm-find-files
@@ -76,16 +75,22 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Hooks
 
+;; Some help from https://www.emacswiki.org/emacs/DelightedModes on getting this
+;; to work with auto-fill-mode.
 (defun shellhead/auto-fill-hook ()
   "Setup auto-fill-mode by setting the fill-column, and only apply to comments,
-diminish (or delight) the mode. Some help from 
-https://www.emacswiki.org/emacs/DelightedModes on getting this to work with
-auto-fill-mode."
+diminish (or delight) the mode."
   (auto-fill-mode 1)
   (set-fill-column 80)
   (setq comment-auto-fill-only-comments t)
   (delight 'auto-fill-function nil t))
+
 (add-hook 'prog-mode-hook #'shellhead/auto-fill-hook)
+
+(defun shellhead/company-hook ()
+  (company-mode 1))
+
+(add-hook 'prog-mode-hook #'shellhead/company-hook)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Packages
@@ -99,18 +104,35 @@ auto-fill-mode."
 ;; TODO Build out personal theme, this or powerline-evil don't provide
 ;; the themes I want.
 (use-package powerline
+  :ensure t
   :config (powerline-default-theme))
 
-(use-package helm-config
+(use-package helm-config		; Narrowing framework.
   :ensure helm
   :demand t
   :config
   (use-package helm
+    :ensure t
     :bind (:map helm-map
                 ("<tab>" . helm-execute-persistent-action)))
-  (setq helm-split-window-in-side-p t)) ; Open helm in current window.
+  (setq helm-split-window-in-side-p t) ; Open helm in current window.
+  (use-package helm-descbinds	       ; Better way to lookup keys.
+    :ensure t))
 
 (use-package org
+  :delight org-mode "org"		; I like lowercase.
   :config
   (add-hook 'org-mode-hook #'shellhead/auto-fill-hook) ; Turn on auto-fill-mode
-  (setq org-hide-leading-stars t)		       ; Hide all but the last star.
+  (setq org-hide-leading-stars t))		       ; Hide all but the last star.
+
+(use-package flycheck
+  :ensure t)
+
+(use-package company			; Autocompletion.
+  :ensure t
+  :bind (:map company-active-map	; Remap active keymap.
+              ("C-h" . company-select-next)
+              ("C-t" . company-select-previous))
+  :config
+  (setq company-minimum-prefix-length 5) ; Look for completions after 5 chars.
+  (setq company-idle-delay 0.25))	 ; Look for completions after 0.25s.
