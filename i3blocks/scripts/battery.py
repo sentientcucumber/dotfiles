4f657665aeacc512887307fb18af17835ca241f1
Michael Hunsinger <mike.hunsinger@gmail.com>
2016-06-15 12:46:18 -0600

    More work on the battery python script. Better formatting.

--- i3blocks/scripts/battery.py
+++ i3blocks/scripts/battery.py
@@ -6,9 +6,13 @@ try:
 except CalledProcessError:
     print('Error running "acpi -b"')
 
-regex = re.compile('^Battery \d+: (?P<state>\w+), (?P<charge>\d+).*$', re.M)
+# Sample "acpi -b" output:
+#   Battery 0: Discharging, 86%, 02:10:07 remaining
+#   Battery 1: Full, 100%
+# Capture the state of the battery (Charging, Discharging, Full or Unknown), the
+# percentage of charge, and time remaining.
+regex = re.compile('^Battery \d+: (\w+), (\d+)%(?:, ([\d:]+))?.*$', re.M)
 batteries = regex.findall(output)
-count = len(batteries)
 sum = 0
 
 for battery in batteries:
@@ -20,5 +24,18 @@ for battery in batteries:
     if state != 'Unknown':
         sum += int(battery[1])
 
+count = len(batteries)
+percentage = int(sum / (count * 100) * 100)
+
 # Display the battery percentage
-print(sum / (count * 100) * 100, '%')
+print('{}%'.format(percentage))
+
+# TODO Add battery icon to act as the label. Change the icon based on the amount
+# of battery left. If plugged in (at least one state is Charging), then display
+# the plug.
+# TODO Change some sort of color based on the amount of battery left. I'm not
+# sure where this would be used (possibly on underline, highlighting the percent
+# left, not really sure). Decide where it would go before spending time on
+# implementation.
+# TODO Calculate the amount of time remaining. Pretty low-priority as I rarely
+# use it, but could be useful to others.
